273420424e03c591e7cf4cfce04ce5287c2caabb
han-gaeul <gmlakdcjs314@gmail.com>
2022-09-30 13:41:54 +0900

    기능 구현

--- pairApp/urls.py
+++ pairApp/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'pairApp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/', views.detail, name='detail'),
-    path('new/', views.new, name='new')
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('create/', views.create, name='create'),
+    path('update/<int:pk>', views.update, name='update'),
+    path('delete/<int:pk>', views.delete, name='delete'),
 ]
--- pairApp/views.py
+++ pairApp/views.py
@@ -1,14 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Review
 
 
 
 def index(request):
-    return render(request, 'pairApp/index.html')
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews' : reviews
+    }
+    return render(request, 'pairApp/index.html', context)
 
 
-def detail(request):
-    return render(request, 'pairApp/detail.html')
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {
+        'review' : review
+    }
+    return render(request, 'pairApp/detail.html', context)
 
 
 def new(request):
+
     return render(request, 'pairApp/new.html')
+
+def edit(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {
+        'review' : review
+    }
+    return render(request, 'pairApp/edit.html', context)
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    Review.objects.create(title=title, content=content)
+
+    return redirect('pairApp:index')
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    review.title = title
+    review.content = content
+    review.save()
+
+    return redirect('pairApp:detail', review.pk)
+
+def delete(request, pk):
+    Review.objects.get(pk=pk).delete() 
+    return redirect('pairApp:index')
